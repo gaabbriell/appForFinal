7ecf6fcb8bca259544a546e49a70391fc6303fa3
AlexGavaketashvili <alexandre.gavaketashvili@gmail.com>
2023-01-03 18:02:36 +0400

    davamate rac shevdzeli

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottomNavView)
+        navController = findNavController(R.id.nav_host_fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.myapplication.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
