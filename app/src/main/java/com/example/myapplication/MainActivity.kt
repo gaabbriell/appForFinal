246108fe3a8d8f8f8193fa06306cf9cfc9d9ed2d
gaabbriell <gabriel.gambashidze.1@btu.edu.ge>
2023-01-07 21:56:22 +0400

    design

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager2.widget.ViewPager2
-import com.example.myapplication.databinding.ActivityMainBinding
+//import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayout
@@ -27,20 +28,23 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button = findViewById(R.id.button)
 
+        button = findViewById(R.id.button)
         bottomNavigationView = findViewById(R.id.bottomNavView)
         navController = findNavController(R.id.nav_host_fragment)
         bottomNavigationView.setupWithNavController(navController)
 
-
     }
+
+//          fun for image edit layout
     public fun replaceFragment(fragment: Fragment){
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragmentContainer, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
     }
+
+
 }
 
 
