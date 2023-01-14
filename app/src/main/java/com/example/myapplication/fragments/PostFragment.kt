11206cdf87cb5dbae7709f961593c18e5010c1b5
NikaKiknadze <kiknadzenika88@gmail.com>
2023-01-15 01:44:23 +0400

    bolomde ar ari damtavrebuli

--- app/src/main/java/com/example/myapplication/fragments/PostFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/PostFragment.kt
@@ -1,49 +1,52 @@
 package com.example.myapplication.fragments
 
-import android.app.Activity.RESULT_OK
-import android.content.Intent
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.Post
+import com.example.myapplication.PostRecyclerAdapter
 import com.example.myapplication.R
 
 
 class PostFragment : Fragment(R.layout.fragment_post) {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerAdapter: PostRecyclerAdapter
     companion object{
         val IMAGE_REQUEST_CODE = 100
     }
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View{
         val rootView: View = inflater.inflate(R.layout.fragment_post, container, false)
-
-        var ButtonChooseImage: Button = rootView.findViewById(R.id.button_choose_image)
-        var postImage: ImageView = rootView.findViewById(R.id.postImage)
-        ButtonChooseImage.setOnClickListener{
-            openFileChooser()
+        var buttonPublish: Button = rootView.findViewById(R.id.buttonPublish)
+        var editTextImageUrl: EditText = rootView.findViewById(R.id.editTextImageUrl)
+        var editTextPostText: EditText = rootView.findViewById(R.id.editTextPostText)
+        buttonPublish.setOnClickListener {
+            val sharedPreferences : SharedPreferences = activity?.applicationContext!!.getSharedPreferences("gmail", Context.MODE_PRIVATE)
+            val editor : SharedPreferences.Editor = sharedPreferences.edit()
+            var x = 0
+            editor.apply{
+                putString("ImageUrl", editTextImageUrl.text.toString())
+                putString("PostText", editTextPostText.text.toString())
+                putInt("PositionNumber", x)
+            }.apply()
+            editTextImageUrl.text = null
+            editTextPostText.text = null
+            x++
         }
 
         return rootView
     }
-
-    private fun openFileChooser() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val postImage: ImageView = requireView().findViewById(R.id.postImage)
-
-        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK)
-            postImage.setImageURI(data?.data)
-        else super.onActivityResult(requestCode, resultCode, data)
-    }
-
 }
